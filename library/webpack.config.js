c1a6c3e2c4350b451d3a51323da2b61743eefb7c
puliwang <puliwang@exiao.tech>
2022-08-16 18:17:08 +0800

    1

--- library/webpack.config.js
+++ library/webpack.config.js
@@ -1,15 +1,16 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: {
     index: {
       import: './src/index.js',
-      dependOn: 'tool',
+      // dependOn: 'tool',
     },
-    tool: {
-      import: './src/tool.js',
-    }
+    // tool: {
+    //   import: './src/tool.js',
+    // }
   },
   mode: 'development',
   output: {
@@ -18,6 +19,17 @@ module.exports = {
   },
   devtool: false,
   plugins: [
-    new CleanWebpackPlugin()
-  ]
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        minify: TerserPlugin.uglifyJsMinify,
+        terserOptions: {
+          comments: false
+        }
+      }),
+    ],
+  },
 };
